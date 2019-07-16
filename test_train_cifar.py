e84c262b496cb321729f691dd2306f9b7f78cb96
brianhhu <brianh@alleninstitute.org>
2019-07-15 22:37:41 -0700

    Fix CIFAR-100 normalization values and add larger ResNet9

--- test_train_cifar.py
+++ test_train_cifar.py
@@ -45,15 +45,15 @@ def train_cifar():
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        Cutout(18),  # add Cutout
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
+        Cutout(18, random_pixel=True),  # add Cutout
+        transforms.Normalize((0.5071, 0.4865, 0.4409),
+                             (0.2673, 0.2564, 0.2762)),
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.5071, 0.4865, 0.4409),
+                             (0.2673, 0.2564, 0.2762)),
     ])
 
     trainset = torchvision.datasets.CIFAR100(
@@ -85,6 +85,7 @@ def train_cifar():
   # Select model here
   # model = BaiduNet8()
   # model = ResNet9(40, 80, 160, 320)
+  # model = ResNet9(64, 128, 256, 512)
   # model = ResNet18()
   model = WRN_McDonnell(20, 10, 100, binarize=True)
 
