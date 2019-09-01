432b0c0e5d4a9dab9d749b7fa8b399cd97f8be61
brianhhu <brianh@alleninstitute.org>
2019-09-01 13:41:05 -0700

    Change EfficientNet num_classes to 100

--- models/EfficientNet.py
+++ models/EfficientNet.py
@@ -56,7 +56,7 @@ class Block(nn.Module):
 
 
 class EfficientNet(nn.Module):
-    def __init__(self, cfg, num_classes=10):
+    def __init__(self, cfg, num_classes=100):
         super(EfficientNet, self).__init__()
         self.cfg = cfg
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3,
