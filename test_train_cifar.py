410f0b093389f87d2b471d1261fc1cb84d0d7ae5
brianhhu <brianh@alleninstitute.org>
2019-09-14 11:44:27 -0700

    Clean up TPU training code by removing unneeded arguments

--- test_train_cifar.py
+++ test_train_cifar.py
@@ -5,8 +5,7 @@ FLAGS = test_utils.parse_common_options(
     batch_size=125,
     num_epochs=254,
     momentum=0.9,
-    lr=0.8,
-    target_accuracy=80.0)
+    lr=0.8)
 
 import os
 import torch
@@ -23,62 +22,49 @@ import torchvision.transforms as transforms
 # Import utilities and models
 from torch.optim.lr_scheduler import MultiStepLR
 from utilities import Cutout, RandomPixelPad, CosineAnnealingRestartsLR
-from models import BaiduNet8, EfficientNetB0, ResNet9, ResNet18, WRN_McDonnell
+from models import WRN_McDonnell
 
 
 def train_cifar():
   print('==> Preparing data..')
 
-  if FLAGS.fake_data:
-    train_loader = xu.SampleGenerator(
-        data=(torch.zeros(FLAGS.batch_size, 3, 32,
-                          32), torch.zeros(FLAGS.batch_size,
-                                           dtype=torch.int64)),
-        sample_count=50000 // FLAGS.batch_size)
-    test_loader = xu.SampleGenerator(
-        data=(torch.zeros(FLAGS.batch_size, 3, 32,
-                          32), torch.zeros(FLAGS.batch_size,
-                                           dtype=torch.int64)),
-        sample_count=10000 // FLAGS.batch_size)
-  else:
-    transform_train = transforms.Compose([
-        # transforms.RandomCrop(32, padding=4),
-        transforms.Lambda(lambda x: RandomPixelPad(x, padding=4)),
-        transforms.RandomCrop(32),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        Cutout(18, random_pixel=True),  # add Cutout
-        transforms.Normalize((0.5071, 0.4865, 0.4409),
-                             (0.2673, 0.2564, 0.2762)),
-    ])
+  transform_train = transforms.Compose([
+    transforms.Lambda(lambda x: RandomPixelPad(x, padding=4)),
+    transforms.RandomCrop(32),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    Cutout(18, random_pixel=True),  # add Cutout
+    transforms.Normalize((0.5071, 0.4865, 0.4409),
+                         (0.2673, 0.2564, 0.2762)),
+  ])
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5071, 0.4865, 0.4409),
-                             (0.2673, 0.2564, 0.2762)),
-    ])
+  transform_test = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.5071, 0.4865, 0.4409),
+                           (0.2673, 0.2564, 0.2762)),
+  ])
 
-    trainset = torchvision.datasets.CIFAR100(
-        root=FLAGS.datadir,
-        train=True,
-        download=True,
-        transform=transform_train)
-    train_loader = torch.utils.data.DataLoader(
-        trainset,
-        batch_size=FLAGS.batch_size,
-        shuffle=True,
-        num_workers=FLAGS.num_workers)
+  trainset = torchvision.datasets.CIFAR100(
+      root=FLAGS.datadir,
+      train=True,
+      download=True,
+      transform=transform_train)
+  train_loader = torch.utils.data.DataLoader(
+      trainset,
+      batch_size=FLAGS.batch_size,
+      shuffle=True,
+      num_workers=FLAGS.num_workers)
 
-    testset = torchvision.datasets.CIFAR100(
-        root=FLAGS.datadir,
-        train=False,
-        download=True,
-        transform=transform_test)
-    test_loader = torch.utils.data.DataLoader(
-        testset,
-        batch_size=FLAGS.batch_size,
-        shuffle=False,
-        num_workers=FLAGS.num_workers)
+  testset = torchvision.datasets.CIFAR100(
+      root=FLAGS.datadir,
+      train=False,
+      download=True,
+      transform=transform_test)
+  test_loader = torch.utils.data.DataLoader(
+      testset,
+      batch_size=FLAGS.batch_size,
+      shuffle=False,
+      num_workers=FLAGS.num_workers)
 
   torch.manual_seed(42)
 
@@ -86,12 +72,7 @@ def train_cifar():
       xm.get_xla_supported_devices(
           max_devices=FLAGS.num_cores) if FLAGS.num_cores !=0 else [])
 
-  # Select model here
-  # model = BaiduNet8()
-  # model = EfficientNetB0()
-  # model = ResNet9(40, 80, 160, 320)
-  # model = ResNet9(64, 128, 256, 512)
-  # model = ResNet18()
+  # Define model here
   model = WRN_McDonnell(20, 10, 100, binarize=True)
 
   # Pass [] as device_ids to run using the PyTorch/CPU engine.
@@ -111,8 +92,6 @@ def train_cifar():
         'scheduler', lambda: CosineAnnealingRestartsLR(
         optimizer, T=2, eta_min=1e-4))
 
-    # tracker = xm.RateTracker()
-
     model.train()
     for x, (data, target) in loader:
       optimizer.zero_grad()
@@ -120,10 +99,8 @@ def train_cifar():
       loss = loss_fn(output, target)
       loss.backward()
       xm.optimizer_step(optimizer)
-      # tracker.add(FLAGS.batch_size)
-      # if x % FLAGS.log_steps == 0:
-      #   print('[{}]({}) Loss={:.5f} Rate={:.2f}'.format(device, x, loss.item(),
-      #                                                   tracker.rate()))
+      if x % FLAGS.log_steps == 0:
+        print('[{}]({}) Loss={:.5f}'.format(device, x, loss.item()))
 
     # Step LR scheduler
     scheduler.step()
@@ -138,8 +115,6 @@ def train_cifar():
       correct += pred.eq(target.view_as(pred)).sum().item()
       total_samples += data.size()[0]
 
-    # print('[{}] Accuracy={:.2f}%'.format(device,
-    #                                      100.0 * correct / total_samples))
     return correct / total_samples
 
   best_accuracy = 0.0
--- test_utils.py
+++ test_utils.py
@@ -1,11 +1,9 @@
 # This module cannot import any other PyTorch/XLA module. Only Python core modules.
 import argparse
-import os
 import sys
 
 
 def parse_common_options(datadir=None,
-                         logdir=None,
                          num_cores=None,
                          batch_size=128,
                          num_epochs=10,
@@ -13,11 +11,9 @@ def parse_common_options(datadir=None,
                          log_steps=20,
                          lr=None,
                          momentum=None,
-                         target_accuracy=None,
                          opts=None):
   parser = argparse.ArgumentParser(add_help=False)
   parser.add_argument('--datadir', type=str, default=datadir)
-  parser.add_argument('--logdir', type=str, default=logdir)
   parser.add_argument('--num_cores', type=int, default=num_cores)
   parser.add_argument('--batch_size', type=int, default=batch_size)
   parser.add_argument('--num_epochs', type=int, default=num_epochs)
@@ -25,9 +21,6 @@ def parse_common_options(datadir=None,
   parser.add_argument('--log_steps', type=int, default=log_steps)
   parser.add_argument('--lr', type=float, default=lr)
   parser.add_argument('--momentum', type=float, default=momentum)
-  parser.add_argument('--target_accuracy', type=float, default=target_accuracy)
-  parser.add_argument('--fake_data', action='store_true')
-  parser.add_argument('--tidy', action='store_true')
   parser.add_argument('--metrics_debug', action='store_true')
   if opts:
     for name, aopts in opts:
